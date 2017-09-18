ad067fd0c91f7e7edb4bf625633d6a4200a99843
Diego Correa <diegocorrea.cc@gmail.com>
2017-09-18 20:33:12 -0300

    adicionando redux e chamada assincrona ao banco

---
+++ frontend/src/todo/todoReducers.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+  description: '',
+  list: []
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED':
+      return { ...state, description: action.payload }
+    break;
+    case 'TODO_SEARCHED':
+      return { ...state, list: action.payload.data }
+    break;
+    default:
+      return state
+
+  }
+}
